1a282152130fce7eb815c7c6d221289e5f71c1a6
AlessandroFava <ales.fafe@gmail.com>
2016-05-06 18:08:18 +0200

    post registrar usuario normal

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var userModel = require('mongoose').model('User');
+var userEmailModel = require('Mongoose').model('Email');
 
 getUsers = function (req, res){
 	console.log("user", req.user);
@@ -12,7 +13,32 @@ getUsers = function (req, res){
 	})
 }
 
+postUserEmail = function (req, res){
+	console.log("user", req.body);
+	var user = {
+		"name" : req.body.name,
+		"email": req.body.email
+	};
+	console.log(user);
+	userEmailModel.findOne({"email": user.email}, function (err, object) {
+		if (err) {
+			console.log(err);
+		} else if (object !== null) {
+			console.log("The user already exists");
+		} else {
+			console.log("The user doesn't exists in the database");
+			user.save(user, function (error) {
+				if (error) {
+					console.log(error);
+				} else {
+					console.log("User saved in database");
+				}
+			})
+		}
+	});
+};
 
 router.get('/', getUsers);
+router.post('/registerUser', postUserEmail);
 
 module.exports = router;
